e90a89171e11797f801aff0658b410098b0f34e5
Gabriel Carvalho <gabriel.carvalho@forpeople.io>
2024-04-10 09:16:24 -0300

    limpeza css

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>Bem-vindo à nossa aplicação!</h1>
+    <p>Esta é a página principal da nossa aplicação.</p>
+  </div>
+</template>
+
+<script setup>
+// Aqui você pode adicionar a lógica do componente
+</script>
+
+<style scoped>
+.home {
+  text-align: center;
+}
+</style>
